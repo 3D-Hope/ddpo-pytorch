f8dff9982872261e543f81befab13945267ca270
Saugat2002 <saugatadhikari.2002@gmail.com>
2026-01-07 19:16:47 +0100

    changed script

--- config/incremental.py
+++ config/incremental.py
@@ -12,7 +12,7 @@ def compressibility():
 
     config.num_epochs = 100
     config.use_lora = True
-    config.save_freq = 100
+    config.save_freq = 1
     config.num_checkpoint_limit = 100000000
 
     # the DGX machine I used had 8 GPUs, so this corresponds to 8 * 8 * 4 = 256 samples per epoch.
