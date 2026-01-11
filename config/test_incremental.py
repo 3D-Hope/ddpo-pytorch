b5d45705258279a135985f256a275df272a2ae1b
Saugat2002 <saugatadhikari.2002@gmail.com>
2026-01-11 08:34:51 +0100

    incremental with better sync

--- config/test_incremental.py
+++ config/test_incremental.py
@@ -15,11 +15,11 @@ def compressibility():
     config.num_checkpoint_limit = 100000000
 
     # the DGX machine I used had 8 GPUs, so this corresponds to 8 * 8 * 4 = 256 samples per epoch.
-    config.sample.batch_size = 8
+    config.sample.batch_size = 4
     config.sample.num_batches_per_epoch = 2
 
     # this corresponds to (8 * 4) / (4 * 2) = 4 gradient updates per epoch.
-    config.train.batch_size = 4
+    config.train.batch_size = 2
     config.train.gradient_accumulation_steps = 1
 
     # prompting
